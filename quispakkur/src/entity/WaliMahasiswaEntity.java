6ab41f9a97f4be6d67582281daa6907a9e55a8e3
M. Rizky Kurniawan Lasim <111512177+JustRizky@users.noreply.github.com>
2023-11-01 10:33:14 +0700

    Delete quispakkur/src/entity/WaliMahasiswaEntity.java

--- quispakkur/src/entity/WaliMahasiswaEntity.java
+++
@@ -1,9 +0,0 @@
-package entity;
-
-public class WaliMahasiswaEntity extends Maba{
-
-    public WaliMahasiswaEntity(String nama, String kewarganegaraan, String telp) {
-        super(nama, kewarganegaraan, telp);
-    }
-    
-}
