a132180b4053c18ce0ffea8f44b6d90c039b3ea1
M. Rizky Kurniawan Lasim <111512177+JustRizky@users.noreply.github.com>
2023-11-01 20:07:43 +0700

    Add files via upload

---
+++ quispakkur/src/entity/EntityWaliMahasiswa.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class EntityWaliMahasiswa extends EntityMaba {
+
+    public EntityWaliMahasiswa(String nama, String kewarganegaraan, String telp) {
+        super(nama, kewarganegaraan, telp);
+    }
+
+}
